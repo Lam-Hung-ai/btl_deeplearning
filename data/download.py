212aea6a82fe206d511a0218c1461b17d5588a31
Lam Hung <2351260653@e.tlu.edu.vn>
2025-12-28 16:40:46 +0700

    update

--- data/download.py
+++ data/download.py
@@ -9,8 +9,9 @@ path = kagglehub.dataset_download("bardiaardakanian/voc0712")
 os.makedirs("data", exist_ok=True)
 
 src = Path(
-    "~/.cache/kagglehub/datasets/bardiaardakanian/voc0712/versions/3/VOC_dataset/VOCdevkit/*"
+    "~/.cache/kagglehub/datasets/bardiaardakanian/voc0712/versions/3/VOC_dataset/VOCdevkit/"
 ).expanduser()
 dst = Path("data/")
-shutil.move(src, dst)
+shutil.move(src / "VOC2007", dst)
+shutil.move(src / "VOC2012", dst)
 print("Đã tải và giải nén bộ dữ liệu VOC 2007 và 2012 vào thư mục data/")
